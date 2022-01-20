da576f2ca5cffdc21d337b057c1849ae0543b235
Benjamin Howell <ben.howell@utexas.edu>
2022-01-19 21:42:29 -0600

    Update phf_functions.py
    adding team and player box score functions to phf_functions.py

--- phf_functions.py
+++ phf_functions.py
@@ -1,3 +1,4 @@
+from urllib import response
 import pandas as pd
 import numpy as np
 import csv
@@ -14,19 +15,36 @@ def phf_pbp(game_id = 368719):
 
 # team = 'tor'
 
-def phf_games(season = 2021):
+def phf_games(season = 2022):
 
     yr = str(season)
 
-    raw = 'https://raw.githubusercontent.com/saiemgilani/fastRhockey-data/main/phf/phf_boxscore.csv'
+    raw = 'https://raw.githubusercontent.com/saiemgilani/fastRhockey-data/main/phf/schedules/csv/phf_schedule_' + yr + '.csv'
     sched = pd.read_csv(raw)
     
     szn = sched[sched.date.str.contains(yr)]
 
     return szn
 
-def phf_player_box(season = 2022):
+def phf_team_box(season = 2022, game_id = 420405):
+    base = 'https://github.com/saiemgilani/fastRhockey-data/blob/main/phf/team_box/csv/team_box_' + str(season) + '.csv.gz'
+    query = {'raw': 'true'}
+    respo = requests.get(base, params=query)
+    fp = BytesIO(respo.content)
 
-    base = (r'https://github.com/saiemgilani/fastRhockey-data/blob/main/phf/player_box/csv/player_box_' + str(season) + '.csv.gz')
+    box = pd.read_csv(fp, compression='gzip')
+    team_box = box[box.game_id == game_id]
 
-    box = pd.read_csv(base)
+    return team_box
+
+def phf_player_box(season = 2022, game_id = 420405):
+
+    base = 'https://github.com/saiemgilani/fastRhockey-data/blob/main/phf/player_box/csv/player_box_' + str(season) + '.csv.gz'
+    query = {'raw': 'true'}
+    respo = requests.get(base, params=query)
+    fp = BytesIO(respo.content)
+
+    box = pd.read_csv(fp, compression='gzip')
+    game_box = box[box.game_id == game_id]
+
+    return game_box
